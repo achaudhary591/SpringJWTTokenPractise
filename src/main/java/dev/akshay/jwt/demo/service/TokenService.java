b90297ab832bb49a97ca2a53cbe6796a3db87866
achaudhary591 <achaudhary591@gmail.com>
2022-12-18 12:34:37 +0530

    done

---
+++ src/main/java/dev/akshay/jwt/demo/service/TokenService.java
@@ -0,0 +1,40 @@
+package dev.akshay.jwt.demo.service;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder jwtEncoder;
+
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generatedToken(Authentication authentication){
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1 , ChronoUnit.HOURS))
+                .subject(authentication.getName())
+                .claim("scope" , scope)
+                .build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
